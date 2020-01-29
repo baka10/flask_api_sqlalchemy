ad86110b1029d887dd7a034ccbd01a5fa19465d5
Bartosz Lipinski <barteklipa112@gmail.com>
2020-01-29 20:46:57 +0100

    App skeleton and initial settings

---
+++ semantive/settings/config.py
@@ -0,0 +1,59 @@
+import configparser
+
+from os.path import abspath, dirname, pardir, join
+
+CURRENT_DIR = abspath(dirname(__file__))
+ROOT_DIR = abspath(join(CURRENT_DIR, pardir))
+
+configfile = configparser.ConfigParser()
+configfile.read(abspath(join(CURRENT_DIR, "secret.cfg")))
+
+
+class Config:
+    SECRET_KEY = configfile.get("env", "secret")
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{}:{}@{}/semantive?charset=utf8mb4".format(
+            configfile.get("db", "username"),
+            configfile.get("db", "password"),
+            configfile.get("db", "host"),
+        )
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 1800
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+    CELERY_DEFAULT_QUEUE = "semantive"
+    CELERY_QUEUES = {
+        CELERY_DEFAULT_QUEUE: {
+            "exchange": CELERY_DEFAULT_QUEUE,
+            "routing_key": CELERY_DEFAULT_QUEUE
+        }
+    }
+    CELERY_TASK_RESULT_EXPIRES = 3600
+    CELERY_IMPORTS = [
+        "semantive.apps.scraper.tasks"
+    ]
+    CELERY_BEAT_SCHEDULE = {
+    }
+    CELERY_TIMEZONE = "Europe/Warsaw"
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    DEBUG_TB_ENABLED = False
+
+
+class DevConfig(Config):
+    ENV = 'dev'
+    DEBUG = True
+    DEBUG_TB_ENABLED = True
+    SQLALCHEMY_ECHO = False
+
+
+ENV = configfile.get("env", "env")
+CONFIGS = {
+    "prod": ProdConfig,
+    "dev": DevConfig
+}.get(ENV, DevConfig)
